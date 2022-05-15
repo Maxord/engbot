b4d836872f33a137c79b93f161613a0c96fbd8e7
Maxord <36801354+Maxord@users.noreply.github.com>
2022-05-15 18:58:08 +0300

    Add files via upload

---
+++ bot1/test.py
@@ -0,0 +1,3 @@
+greetmsg = "Друзья!\n\nВы хотите развить умение общаться на английском языке и интересные и современные темы, а также потренироваться в выполнении заданий в формате ЕГЭ? Тогда вам сюда!"
+lessonmsg = "Начало урока\n\nПеред тем, как урок начнётся, давайте отгадаем тему урока.\n\nunscramble the phrase - nrgaeid dan piaskneg obatu ceecsin "
+aimmsg = "The aim of the lesson is to teach you to select the necessary information from a text for aural comprehension and use it in explanatory monologues of your own. You are going to discuss some scientific issues."
